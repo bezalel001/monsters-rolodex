cf35b3e1b6c07295d5b51255c4fdbccc9823f327
bezalel001 <mickeybezalel@gmail.com>
2019-08-20 05:55:24 +0100

    Updates

---
+++ precache-manifest.2fef2e7ba2e6f08d6790e3c29d5ee806.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42b9e56a0ba6c0f8e581e2a3f660d466",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f1479b5609805ef99846",
+    "url": "/monsters-rolodex/static/css/main.ebc78f0d.chunk.css"
+  },
+  {
+    "revision": "d81979aa8ac68af22713",
+    "url": "/monsters-rolodex/static/js/2.4ea1ec15.chunk.js"
+  },
+  {
+    "revision": "f1479b5609805ef99846",
+    "url": "/monsters-rolodex/static/js/main.ca8805ee.chunk.js"
+  },
+  {
+    "revision": "c7f991e6cb4746d6bd30",
+    "url": "/monsters-rolodex/static/js/runtime~main.205b164d.js"
+  }
+]);
